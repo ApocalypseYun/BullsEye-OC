a7fac4c86a3d395607948a7b5b53d85786cf192c
ApocalypseYun <lgz970108@163.com>
2018-05-07 23:40:52 +0800

    Initial Commit

---
+++ BullsEye-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BullsEye-OC
+//
+//  Created by 刘冠中 on 2018/5/7.
+//  Copyright © 2018年 刘冠中. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BullsEye-OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BullsEye-OC
+//
+//  Created by 刘冠中 on 2018/5/7.
+//  Copyright © 2018年 刘冠中. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
